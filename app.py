bc4d14b5037643b66c059cdd2b531384baf7554d
KEERTHIKA K <keerthikakaruna2005@gmail.com>
2026-06-24 22:45:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 
-model = joblib.load("models/load_forecast_model.pkl")
+model = joblib.load("load_forecast_model.pkl")
 
 st.title("⚡ AI-Powered Intelligent Load Forecasting System")
 
